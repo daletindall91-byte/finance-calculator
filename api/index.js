e9b9bc87a9456e3ef6c721ec478ecc43833c9d45
daletindall91-byte <daletindall91@gmail.com>
2026-07-04 23:06:32 +0100

    Make Vercel adapter load existing server

--- api/index.js
+++ api/index.js
@@ -1,5 +1,35 @@
-import { handleRequest } from "../server.js";
+import http from "node:http";
+import { syncBuiltinESMExports } from "node:module";
+
+let requestHandler;
+const originalCreateServer = http.createServer;
+
+http.createServer = function createServerForVercel(handler) {
+  requestHandler = handler;
+  return {
+    listen() {
+      return this;
+    },
+    on() {
+      return this;
+    },
+    close(callback) {
+      if (callback) callback();
+      return this;
+    },
+  };
+};
+
+syncBuiltinESMExports();
+await import("../server.js");
+http.createServer = originalCreateServer;
+syncBuiltinESMExports();
 
 export default function handler(req, res) {
-  return handleRequest(req, res);
+  if (!requestHandler) {
+    res.statusCode = 500;
+    res.end("Server handler was not initialised.");
+    return;
+  }
+  return requestHandler(req, res);
 }
